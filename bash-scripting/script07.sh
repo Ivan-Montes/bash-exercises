60f356642629f97b50fa9a32d38da3f3b1bf7fdd
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2025-10-18 07:26:47 +0200

    refactor: improve script07

--- bash-scripting/script07.sh
+++ bash-scripting/script07.sh
@@ -3,47 +3,106 @@ set -euo pipefail
 #set -x
 
 
-validate_params() {
+check_args() {
 
   if (( $# != 2 )); then
-    echo -e "\tError: Exactly two parameters are required." >&2
-    return 1
+    echo -e "\tError: we need 2 args" >&2
+    exit 1
   fi
+}
 
-  local archive="${1}"
-  if ! [ -r "${archive}" ]; then
-    echo -e "\tError: The first parameter must be a readable file: $archive" >&2
-    return 1
+validate_args() {
+
+  local file="$1"
+  local dir="$2"
+  
+  validate_file "$file"
+  validate_dir "$dir"
+}
+
+validate_file() {
+
+  local file="$1"
+  
+  if ! [[ -f "$file" ]]; then
+    echo -e "\tError: file invalid => $file" >&2
+    exit 1
   fi
-
-  local directory="${2}"
-  if ! [ -d "${directory}" ]; then
-    echo -e "\tError: The second parameter must be a directory: $directory" >&2
-    return 1
+  
+  if ! [[ -r "$file" ]]; then
+    echo -e "\tError: READ permission is required => $file" >&2
+    exit 1
   fi
+}
 
-  if ! [ -w "${directory}" ]; then
-    echo -e "\tError: The directory must be writable: $directory" >&2
-    return 1
+validate_dir() {
+
+  local dir="$1"
+  
+  if ! [[ -d "$dir" ]]; then
+    echo -e "\tError: Route must be a valid directory => $dir" >&2
+    exit 1
   fi
-
+  
+  if ! [[ -r "$dir" ]]; then
+    echo -e "\tError: READ permission is required => $dir" >&2
+    exit 1
+  fi
+  
+  if ! [[ -w "$dir" ]]; then
+    echo -e "\tError: WRITE permission is required => $dir" >&2
+    exit 1
+  fi
+  
+  if ! [[ -x "$dir" ]]; then
+    echo -e "\tError: EXECUTE permission is required => $dir" >&2
+    exit 1
+  fi
 }
 
 copy_file() {
 
-  local archive="${1}"
-  local directory="${2}"
+  local file="$1"
+  local dir="$2"
+  
+  if cp "$file" "$dir"; then
+    echo -e "\tCopy successfully: $file => $dir"
+  else
+    die "Error on copy: $file => $dir"
+  fi
+}
 
-  echo -e "\tCopy ${archive} to ${directory}"
-  cp "${archive}" "${directory}" && echo -e "\tCopy successfully" || echo -e "\tError on copy process"
+die() {
 
+  echo -e "\t$*" >&2
+  exit 1
+}
+
+mod_permissions() {
+
+  local file="$1"
+  local dir="$2"
+  local route_with_file="$dir/$file"
+
+  if chmod ug+x "$route_with_file"; then
+    echo -e "\tPermissions added: $route_with_file (user/group executable)"
+  else
+    die "Failed to add permissions: $route_with_file"
+  fi
+
+  if chmod o-x "$route_with_file"; then
+    echo -e "\tPermissions removed: $route_with_file (others no longer executable)"
+  else
+    die "Failed to remove permissions: $route_with_file"
+  fi
 }
 
 main() {
 
-  validate_params "${@}"
-  copy_file "${@}"
-
+  check_args "$@"
+  validate_args "$@"
+  copy_file "$@"
+  mod_permissions "$@"
 }
 
-main "${@}"
+main "$@"
